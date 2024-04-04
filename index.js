2592f6f30428565c80b645c41c43ccbb0de35b9f
man lok lee <lok@pg95.com>
2024-04-04 16:33:45 +0000

    finished

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:word?", function(req, res){
+  console.log(req.params.word)
+}) 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
